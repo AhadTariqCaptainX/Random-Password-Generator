22bf2315e3482307be451a9fc6b01663f93fca96
AhadTariqCaptainX <ahad.imvu@gmail.com>
2025-07-24 06:45:48 +0500

    remove repeating error

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ const characters = [
     ",","|",":",";","<",">",".","?","/"
 ];
 
-let pass1 = " "
-let pass2 = " "
-let password1 = document.getElementById("pass1")
-let password2 = document.getElementById("pass2")
+
 
 function randomPasswordGenerator(){
-    
-for(let i=0; i<15; i++){
-    let p1 = Math.floor(Math.random()*characters.length)
-    let p2 = Math.floor(Math.random()*characters.length)
-      pass1 += characters[p1]
-      pass2 += characters[p2]
- }
- password1.textContent = pass1
- password2.textContent = pass2
+    let pass1 = " "
+    let pass2 = " "
+    let password1 = document.getElementById("pass1")
+    let password2 = document.getElementById("pass2")
+    for(let i=0; i<15; i++){
+        let p1 = Math.floor(Math.random()*characters.length)
+        let p2 = Math.floor(Math.random()*characters.length)
+        pass1 += characters[p1]
+        pass2 += characters[p2]
+    }
+    password1.textContent = pass1
+    password2.textContent = pass2
 }
 
